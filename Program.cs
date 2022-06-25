51a6c2860b01a4d7616a605f17f8172d4f29eac9
yoniT02 <yonastaye859@gmail.com>
2022-06-25 21:51:39 +0300

    Finished Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* PROGRAM THAT TAKES AND SORTS USER INPUT */
+
+Console.Write("Enter amount of numbers to be sorted: ");
+int size = int.Parse(Console.ReadLine());
+int[] array = new int[size];
+
+for (int i = 0; i < size; i++)
+{
+    Console.Write("Enter number " + (i + 1) + ": ");
+    array[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < size; i++)
+{
+    for (int j = 0; j < i; j++)
+    {
+        if (array[j] > array[i])
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
+
+Console.Write("Sorted array: ");
+for (int i = 0; i < size; i++)
+    Console.Write(array[i] + " ");
